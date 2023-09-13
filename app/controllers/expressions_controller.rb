f5b0c3b286e7427a9d50673655bb17d5bd9b614a
Amir Khan <ak2022dev@gmail.com>
2023-09-13 14:32:18 +0100

    Refactor move one hour from now time expression into controller then use instance variable in view

--- app/controllers/expressions_controller.rb
+++ app/controllers/expressions_controller.rb
@@ -2,5 +2,6 @@ class ExpressionsController < ApplicationController
   def expressions
     @addition = 1 + 2
     @concat = "cow" + "boy"
+    @one_hour_from_now = 1.hour.from_now.localtime
   end
 end
